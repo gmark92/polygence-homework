1683805382179fb5b96e8d9bec4b0fa41587d5bb
Mark Gulacsy <mark.gulacsy@gmail.com>
2020-08-31 14:38:48 +0200

    Setup allowed hosts

--- spendingtracker/settings.py
+++ spendingtracker/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '@sa2shwnv%@#n#t7jefr_$k54y$*ixkn*arlslvri=b19)j#b-'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
