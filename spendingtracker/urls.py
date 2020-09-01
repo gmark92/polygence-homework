a966b0a04e8062095457812db73863cec2e38e20
Mark Gulacsy <mark.gulacsy@gmail.com>
2020-09-01 08:31:55 +0200

    Add new endpoint for spendings
    api/me/spendings/

--- spendingtracker/urls.py
+++ spendingtracker/urls.py
@@ -14,8 +14,23 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from rest_framework import routers
+import spendings.views
+
+me_router = routers.SimpleRouter()
+me_router.register(
+    'spendings', spendings.views.SpendingViewSetForUser, basename='spending')
+
+me_urls = [
+    path('', include(me_router.urls)),
+]
+
+api_urls = [
+    path('me/', include((me_urls, 'me')))
+]
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api/', include((api_urls, 'api'))),
 ]
